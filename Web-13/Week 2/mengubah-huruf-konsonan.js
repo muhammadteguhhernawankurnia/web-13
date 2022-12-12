14d25ca4784e61d0155c15cdc06f4c36301a7ce1
Teguh <94585470+muhammadteguhhernawankurnia@users.noreply.github.com>
2022-12-12 22:53:52 +0700

    Delete mengubah-huruf-konsonan.js

--- Web-13/Week 2/mengubah-huruf-konsonan.js
+++
@@ -1,9 +0,0 @@
-const word = "jakarta";
-
-if (typeof word !== "string") {
-  console.log("Tipe Data Teks Harus String!");
-} else {
-  let replaceWord = word.replace(/a/g, "o");
-
-  console.log(replaceWord);
-}
